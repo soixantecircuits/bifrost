5d8cf38af977240c2c0053646987ddfc4387cb9c
gabrielstuff <gabriel@soixantecircuits.fr>
2016-07-22 10:59:07 +0200

    add nvmrc

---
+++ test/server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+app.get('*', (req, res) => res.send(200, 'hello test server'))
+app.post('/', function (req, res) {
+  console.log('POST on /')
+  res.send(200, 'ok')
+})
+app.post('/test', function (req, res) {
+  console.log('POST on /test')
+  res.send(200, 'ok')
+})
+
+app.listen(5656)
