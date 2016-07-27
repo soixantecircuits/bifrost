5ec59e01e184c084816798e6fee0ebe9f1717083
getupandgo <getupandgo11@gmail.com>
2016-07-27 17:45:01 +0300

    added mdns to test server

--- test/server.js
+++ test/server.js
@@ -3,13 +3,25 @@ var bodyParser = require('body-parser')
 var _ = require('lodash')
 var path = require('path')
 var multer = require('multer')
+var mdns = require('mdns')
 
 var app = express()
+
+var browser = mdns.createBrowser(mdns.tcp('bifrost'))
+
 var upload = multer({
   dest: path.join(__dirname, './uploads')
 })
 
 var failed = false
+
+browser.on('serviceUp', function (service) {
+  console.log("server.js: service up: ", service)
+})
+
+browser.on('serviceDown', function (service) {
+  console.log("server.js: service down: ", service)
+})
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -20,33 +32,33 @@ app.get('*', function (req, res) {
 })
 
 app.post('/file', upload.any(), function (req, res) {
-  console.log('POST on /file')
+  console.log('test.js: POST on /file')
   if (req.files) {
-    console.log('with files')
+    console.log('test.js: with files')
     _.forEach(req.files, function (file) {
       console.log(file.originalname)
     })
   } else {
-    console.log('with empty file field')
+    console.log('test.js: with empty file field')
   }
   res.status(200).send('ok')
 })
 
 app.post('/base', function (req, res) {
-  console.log('POST on /base with ')
-  req.data ? console.log('base64 in data field') : console.log('empty data field')
+  console.log('test.js: POST on /base with ')
+  req.data ? console.log('test.js: base64 in data field') : console.log('test.js: empty data field')
 
   res.status(200).send('ok')
 })
 
 app.post('/form', function (req, res) {
-  console.log('POST on /form with ')
+  console.log('test.js: POST on /form with ')
   if (req.body) {
     _.forEach(req.body, function (field) {
-      console.log('field value', field)
+      console.log('test.js: field value', field)
     })
   } else {
-    console.log('empty body')
+    console.log('test.js: empty body')
   }
 
   res.status(200).send('ok')
@@ -54,17 +66,17 @@ app.post('/form', function (req, res) {
 
 app.post('/fail', function (req, res) {
   if (!failed) {
-    console.log('POST on /fail, sending error and waiting for bifrost posting')
+    console.log('test.js: POST on /fail, sending error and waiting for bifrost posting')
     failed = true
     res.status(500).send('fail')
   } else {
-    console.log('request again from bifrost, sending 200')
+    console.log('test.js: request again from bifrost, sending 200')
     res.status(200).send('ok')
   }
 })
 
 app.post('/response', function (req, res) {
-    console.log('getting response from bifrost')
+    console.log('test.js: getting response from bifrost')
 })
 
 app.post('/test', function (req, res) {
@@ -72,4 +84,10 @@ app.post('/test', function (req, res) {
   res.status(200).send('ok')
 })
 
-app.listen(5656)
+app.listen(5656, function () {
+  try {
+    browser.start()
+  } catch (err) {
+    console.log('test.js: mdns error -', err)
+  }
+})
